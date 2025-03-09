921700032db0a37fb5a77888990a3acf1102a518
Britton Robitzsch <b@robitzs.ch>
2025-03-08 19:49:55 -0700

    Update iwd clanModule to use vars instead of secrets/facts

--- clanModules/iwd/roles/default.nix
+++ clanModules/iwd/roles/default.nix
@@ -4,34 +4,11 @@
   pkgs,
   ...
 }:
-
 let
   cfg = config.clan.iwd;
-  secret_path = ssid: config.clan.core.facts.services."iwd.${ssid}".secret."iwd.${ssid}".path;
-  secret_generator = name: value: {
-    name = "iwd.${value.ssid}";
-    value =
-      let
-        secret_name = "iwd.${value.ssid}";
-      in
-      {
-        secret.${secret_name} = { };
-        generator.prompt = "Wifi password for '${value.ssid}'";
-        # ref. man iwd.network
-        generator.script = ''
-          config="
-          [Settings]
-            AutoConnect=${if value.AutoConnect then "true" else "false"}
-          [Security]
-            Passphrase=$(echo -e "$prompt_value" | ${lib.getExe pkgs.gnused} "s=\\\=\\\\\\\=g;s=\t=\\\t=g;s=\r=\\\r=g;s=^ =\\\s=")
-          "
-          echo "$config" > "$secrets/${secret_name}"
-        '';
-      };
-  };
 in
 {
-  options.clan.iwd = {
+  options.clan.iwd= {
     networks = lib.mkOption {
       type = lib.types.attrsOf (
         lib.types.submodule (
@@ -56,33 +33,45 @@ in
       description = "Wifi networks to predefine";
     };
   };
-
-  imports = [
-    (lib.mkRemovedOptionModule [
-      "clan"
-      "iwd"
-      "enable"
-    ] "Just define clan.iwd.networks to enable it")
-  ];
-
+  
   config = lib.mkMerge [
     (lib.mkIf (cfg.networks != { }) {
-      # Systemd tmpfiles rule to create /var/lib/iwd/example.psk file
-      systemd.tmpfiles.rules = lib.mapAttrsToList (
-        _: value: "C /var/lib/iwd/${value.ssid}.psk 0600 root root - ${secret_path value.ssid}"
+      # Setup generators for WiFi passwords
+      clan.core.vars.generators = lib.mapAttrs' (name: value:
+        lib.nameValuePair "wifi-${value.ssid}" {
+          prompts.password = {
+            description = "WiFi password for '${value.ssid}'";
+            type = "hidden";
+          };
+          files.config = {
+            secret = true;
+          };
+          runtimeInputs = [ pkgs.coreutils ];
+    script = ''
+            # Get the password and escape special characters
+            PASSWORD=$(cat $prompts/password | ${lib.getExe pkgs.gnused} 's/\\/\\\\/g; s/\t/\\t/g; s/\r/\\r/g; s/^ /\\s/g')
+            
+            # Create a properly formatted IWD network configuration file
+            cat > "$out/config" << EOF
+            [Settings]
+              AutoConnect=${if value.AutoConnect then "true" else "false"}
+            [Security]
+              Passphrase=$PASSWORD
+            EOF
+          '';        }
       ) cfg.networks;
-
-      clan.core.facts.services = lib.mapAttrs' secret_generator cfg.networks;
-
-      # TODO: restart the iwd.service if something changes
+      
+      # Create symlinks to the generated configuration files
+      systemd.tmpfiles.rules = lib.mapAttrsToList (
+        _: value: 
+        "C /var/lib/iwd/${value.ssid}.psk 0600 root root - ${config.clan.core.vars.generators."wifi-${value.ssid}".files.config.path}"
+      ) cfg.networks;
     })
     {
       # disable wpa supplicant
       networking.wireless.enable = false;
-
       # Set the network manager backend to iwd
       networking.networkmanager.wifi.backend = "iwd";
-
       # Use iwd instead of wpa_supplicant. It has a user friendly CLI
       networking.wireless.iwd = {
         enable = true;
